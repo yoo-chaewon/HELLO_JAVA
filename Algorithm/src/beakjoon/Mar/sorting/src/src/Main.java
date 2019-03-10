b9556bdd75d06c8fe8f92711e34e281f8e40e0f5
yoo-chaewon <ohwow5@sookmyung.ac.kr>
2019-03-10 22:23:57 +0900

    bubble sorting

--- Algorithm/src/beakjoon/Mar/sorting/src/src/Main.java
+++ Algorithm/src/beakjoon/Mar/sorting/src/src/Main.java
@@ -7,29 +7,43 @@ public class Main {
         int count = sc.nextInt();
         int[] arr = new int[count];
 
-        for (int i = 0; i < count; i++){
+        for (int i = 0; i < count; i++) {
             arr[i] = sc.nextInt();
         }
         Main obj = new Main();
-        obj.SelectSort(count, arr);
+        //obj.SelectSort(count, arr);
+        obj.BubbleSort(count, arr);
 
 
-        for (int i = 0 ; i < count; i++) {
+        for (int i = 0; i < count; i++) {
             System.out.println(arr[i]);
         }
     }
 
-    public int[] SelectSort(int count, int[] arr){
-        int cur_x = 0;
-        int cur_m = cur_x + 1;
+    public int[] SelectSort(int count, int[] arr) {
         int temp;
 
-        for (int i = 0; i < count; i++){
-            for (int j = i+1 ; j < count; j++){
-                if (arr[i] > arr[j]){
+        for (int i = 0; i < count; i++) {
+            for (int j = i + 1; j < count; j++) {
+                if (arr[i] < arr[j]) {
                     temp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public int[] BubbleSort(int count, int[] arr) {
+        int temp;
+
+        for (int i = 1; i < count; i++) {
+            for (int j = 0; j < count - i; j++) {
+                if (arr[j] > arr[j+1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
         }
