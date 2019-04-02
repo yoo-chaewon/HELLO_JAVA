179d1df8a77e532ea99ca2a1760abb6d207de6f8
yoo-chaewon <ohwow5@sookmyung.ac.kr>
2019-04-02 21:50:39 +0900

    1260 BFS성공

--- Algorithm/src/beakjoon/Mar/queue/src/Main.java
+++ Algorithm/src/beakjoon/Mar/queue/src/Main.java
@@ -13,7 +13,8 @@ public class Main {
         try {
             //obj.QueueBasicMain();
             //obj.PrintQueueMain();
-            obj.JooMain();
+            //obj.JooMain();
+            obj.DFSBFSMain();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -164,5 +165,48 @@ public class Main {
             }
         }
         System.out.print(">");
+    }
+
+    //1260_DFS,BFS
+    int[][] map = new int[1001][10001];
+    Boolean[] visited = new Boolean[1001];
+    int N;
+    int M;
+    public void DFSBFSMain() throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] f_line = bufferedReader.readLine().split(" ");
+        N = Integer.parseInt(f_line[0]);//정점의 개수
+        M = Integer.parseInt(f_line[1]);//간선의 개수
+        int V = Integer.parseInt(f_line[2]);//시작할 정점의 번호
+
+        for (int i = 1; i <= M; i++){
+            String[] node = bufferedReader.readLine().split(" ");
+            int A = Integer.parseInt(node[0]);
+            int B = Integer.parseInt(node[1]);
+            map[A][B] = 1;
+            map[B][A] = 1;
+        }
+        for (int j = 1; j <= N; j++){
+            visited[j] = false;
+        }
+        BFS(V);
+    }
+    public void BFS(int V){
+        LinkedList<Integer> queue = new LinkedList<>();
+        queue.offer(V);
+        visited[V] = true;
+        System.out.print(V + " ");
+
+        while (!queue.isEmpty()){
+            int tmp = queue.peek();
+            for (int i = 1; i <= N; i++){
+                if ((map[tmp][i] == 1) && (visited[i]==false)){
+                    visited[i] = true;
+                    queue.offer(i);
+                    System.out.print(i + " ");
+                }
+            }
+            queue.poll();
+        }
     }
 }
