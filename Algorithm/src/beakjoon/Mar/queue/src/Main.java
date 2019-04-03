2c1d85b09fa4aae9a64f373a4d94cb0addd14901
yoo-chaewon <ohwow5@sookmyung.ac.kr>
2019-04-03 10:58:53 +0900

    add April file

--- Algorithm/src/beakjoon/Mar/queue/src/Main.java
+++ Algorithm/src/beakjoon/Mar/queue/src/Main.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
 
