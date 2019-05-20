30bf3e5f0ab3e0698bd4ff2e472a5c75128f7ddf
yoo-chaewon <ohwow5@sookmyung.ac.kr>
2019-05-20 21:10:43 +0900

    Solve Q_1753-PriorityQueue

--- Algorithm/Dijkstra/src/com/company/Q_1753_PQ.java
+++ Algorithm/Dijkstra/src/com/company/Q_1753_PQ.java
@@ -1,75 +1,72 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.PriorityQueue;
-import java.util.Scanner;
 
 public class Q_1753_PQ {
-    static int INF = 99;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int vertex = sc.nextInt();
-        int edge = sc.nextInt();
-        int start = sc.nextInt();
+    static int INF = 214783647;
+    public static void main(String[] args) throws Exception{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] VE = bufferedReader.readLine().split(" ");
+        int vertex = Integer.parseInt(VE[0]);
+        int edge = Integer.parseInt(VE[1]);
 
-        int[] distance = new int[vertex + 1];
-        boolean[] visited = new boolean[vertex + 1];
-
-        ArrayList<Edge>[] a = new ArrayList[vertex + 1];
-        for (int i = 1; i <= vertex; i++) {
-            distance[i] = INF;
-            a[i] = new ArrayList<Edge>();
+        int start = Integer.parseInt(bufferedReader.readLine());
+        ArrayList<Node>[] arrayList = new ArrayList[vertex+1];
+        for (int i = 1; i < vertex+1; i++){
+            arrayList[i] = new ArrayList<Node>();
         }
+        for (int i = 0; i < edge; i++){
+            String[] data = bufferedReader.readLine().split(" ");
+            int from = Integer.parseInt(data[0]);
+            int to = Integer.parseInt(data[1]);
+            int weight = Integer.parseInt(data[2]);
+            arrayList[from].add(new Node(to, weight));
+        }
+        int[] distance = new int[vertex+1];
+        Boolean[] visited = new Boolean[vertex+1];
+        Arrays.fill(distance, INF);
+        Arrays.fill(visited, false);
         distance[start] = 0;
 
-        for (int i = 0; i < edge; i++) {
-            int from = sc.nextInt();
-            int to = sc.nextInt();
-            int w = sc.nextInt();
-            a[from].add(new Edge(to, w));
-        }
-        sc.close();
-
-        PriorityQueue<Edge> pq = new PriorityQueue<Edge>();
-        pq.offer(new Edge(start, 0));
-
-        while (!pq.isEmpty()) {
-            Edge current = pq.poll();
-            if (visited[current.dest]) {
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+        queue.offer(new Node(start, 0));
+        while (!queue.isEmpty()){
+            Node node = queue.poll();
+            if (visited[node.dest]){
                 continue;
             }
-            visited[current.dest] = true;
-            for (Edge next : a[current.dest]) {
-                if (visited[next.dest] == false) {
-                    distance[next.dest] = Math.min(distance[next.dest], distance[current.dest] + next.weight);
-                    pq.offer(new Edge(next.dest, distance[next.dest]));
+            visited[node.dest] = true;
+            for (Node next : arrayList[node.dest]){
+                if (visited[next.dest] == false){
+                    distance[next.dest] = Math.min(distance[next.dest], distance[node.dest] + next.weight);
+                    queue.offer(new Node(next.dest, distance[next.dest]));
                 }
             }
         }
 
-        for (int i = 1; i <= vertex; i++) {
-            if (distance[i] == INF) {
+        for (int i = 1; i < vertex+1; i++){
+            if (distance[i] == INF){
                 System.out.println("INF");
-            } else {
+            }else {
                 System.out.println(distance[i]);
             }
         }
-        sc.close();
     }
 }
-
-class Edge implements Comparable<Edge> {
+class Node implements Comparable<Node>{
     int dest;
     int weight;
-
-    Edge(int dest, int weight) {
+    Node(int dest, int weight){
         this.dest = dest;
         this.weight = weight;
     }
-
     @Override
-    public int compareTo(Edge o) {
-        return this.weight - o.weight;
+    public int compareTo(Node o) {
+        return this.weight > o.weight ? 1: -1;
     }
 }
 
