79b37cf4475eb7a5b80e1884da2ba482a10f5179
yoo-chaewon <ohwow5@sookmyung.ac.kr>
2019-05-28 01:55:57 +0900

    Solve Q_1865

--- Algorithm/Dijkstra/src/com/company/Q_1865.java
+++ Algorithm/Dijkstra/src/com/company/Q_1865.java
@@ -1,17 +1,13 @@
 package com.company;
 
-import org.omg.PortableInterceptor.INACTIVE;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.PriorityQueue;
 
 public class Q_1865 {
     static ArrayList<Dot>[] arrayLists;
     static int N;
-    static int MAX = Integer.MAX_VALUE;
     public static void main(String[] args) throws Exception{
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int test_case = Integer.parseInt(bufferedReader.readLine());
@@ -34,59 +30,38 @@ public class Q_1865 {
                 arrayLists[node1].add(new Dot(node2, cost));
                 arrayLists[node2].add(new Dot(node1, cost));
             }
-
-            int w_start = 0;
-            int w_dest = 0;
-            int w_cost = 0;
             for (int c = 0; c < W; c++){
                 String[] w_data = bufferedReader.readLine().split(" ");
-                w_start = Integer.parseInt(w_data[0]);
-                w_dest = Integer.parseInt(w_data[1]);
-                w_cost = Integer.parseInt(w_data[2]);
-                arrayLists[w_start].add(new Dot(w_dest, w_cost));
+                int w_start = Integer.parseInt(w_data[0]);
+                int w_dest = Integer.parseInt(w_data[1]);
+                int w_cost = Integer.parseInt(w_data[2]);
+                arrayLists[w_start].add(new Dot(w_dest, w_cost*-1));
             }
-            int result = (w_cost*-1) + WormHole(w_dest, w_start);
-            if (result < 0){
-                System.out.println("YES");
-            }else {
-                System.out.println("NO");
-            }
-        }
-    }
-    public static int WormHole(int start, int dest){
-        boolean[] visited = new boolean[N+1];
-        int[] distance = new int[N+1];
-        Arrays.fill(distance, MAX);
+            boolean check = false;
+            int[] distance = new int[N+1];
 
-        PriorityQueue<Dot> queue = new PriorityQueue<>();
-        queue.offer(new Dot(start, 0));
-        distance[start] = 0;
-        while (!queue.isEmpty()){
-            Dot current = queue.poll();
-            if (visited[current.dest]){
-                continue;
-            }
-            visited[current.dest] = true;
-            for (Dot next: arrayLists[current.dest]){
-                if (visited[next.dest] == false) {
-                    distance[next.dest] = Math.min(distance[next.dest], distance[current.dest] + next.cost);
-                    queue.offer(new Dot(next.dest, distance[next.dest]));
+            Arrays.fill(distance, Integer.MAX_VALUE);
+            distance[1] = 0;//1이 시작점
+            for (int i = 1; i <= N; i++){//N-1번 하는데, N번째 바뀌면 순환 있는 것.
+                for (int j = 1; j <= N; j++){//노드 1-N 순서대로 보기
+                    for (Dot next: arrayLists[j]){
+                        if (distance[j] == Integer.MAX_VALUE) continue;
+                        if (distance[next.dest] > distance[j] + next.cost){
+                            distance[next.dest] = distance[j] + next.cost;
+                            if (i == N) check = true;
+                        }
+                    }
                 }
             }
+            System.out.println(check? "YES": "NO");
         }
-        return distance[dest];
     }
 }
-class Dot implements Comparable<Dot>{
+class Dot{
     int dest;
     int cost;
     Dot(int dest, int cost){
         this.dest = dest;
         this.cost = cost;
-    }
-
-    @Override
-    public int compareTo(Dot o) {
-        return this.cost > o.cost ? 1: -1;
     }
 }
