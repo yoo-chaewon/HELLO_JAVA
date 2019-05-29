68e4e87a81f46e95a3d93ab2587c111b57decf66
yoo-chaewon <ohwow5@sookmyung.ac.kr>
2019-05-29 18:14:21 +0900

    solve Q_5

--- Algorithm/test0526/src/com/company/Q_5.java
+++ Algorithm/test0526/src/com/company/Q_5.java
@@ -2,43 +2,53 @@ package com.company;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Q_5 {
-    static int N;
     static int M;
+    static int N;
     static int[][] map;
+    static int max = -1;
 
     public static void main(String[] args) throws Exception {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(bufferedReader.readLine());//세로
-        M = 4; //가로
-        map = new int[N + 1][M + 1];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N + 1][M];
+
         for (int i = 0; i < N; i++) {
-            String[] input = bufferedReader.readLine().split(" ");
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(input[j]);
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        int min = Integer.MAX_VALUE;
 
         for (int i = 0; i < M; i++) {
-            min = Math.min(min, DFS(i, 0));
+            DFS(map[0][i], i, 0);
         }
-        System.out.println(min);
+        System.out.println(max);
     }
 
-    public static int DFS(int x, int y) {
-        if (x >= M || y >= N){
-            return map[y][x];
+    public static void DFS(int sum, int x, int y) {
+        if (y == N-1) {
+            max = Math.max(max, sum);
+            return;
         }
-        for (int i = 0; i < M; i++){
-            if (i == x){
+        for (int i = 0; i < M; i++) {
+            if (x == i) {
                 continue;
             }
-            DFS(i, y+1);
+            DFS(sum + map[y+1][i], i, y + 1);
         }
-
-
-        return -1;
     }
 }
+/*
+*
+3 4
+1 2 3 5
+5 6 7 8
+4 3 2 1
+*
+* */
