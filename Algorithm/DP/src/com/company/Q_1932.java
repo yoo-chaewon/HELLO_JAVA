38f8e2fb93ba4d65caba1f475bae090244e6fed4
yoo-chaewon <ohwow5@sookmyung.ac.kr>
2019-05-30 21:31:44 +0900

    Solve Q_1932-이해 완료

--- Algorithm/DP/src/com/company/Q_1932.java
+++ Algorithm/DP/src/com/company/Q_1932.java
@@ -5,16 +5,15 @@ import java.util.Scanner;
 public class Q_1932 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        int num = scanner.nextInt();
+        int[][] map = new int[num+1][num+1];
         int max = -1;
-        int[][] map = new int[n+1][n+1];
-
-        for (int i = 1; i <= n; i++){
-            for (int j =1; j <= i; j++){
-                map[i][j] = scanner.nextInt();
-                if (j == 1) map[i][j] = map[i-1][j] + map[i][j];
-                else if (j == i) map[i][j] = map[i-1][j-1] + map[i][j];
-                else map[i][j] = Math.max(map[i-1][j-1], map[i-1][j]) + map[i][j];
+        for (int i = 1; i < num+1; i++){
+            for (int j = 1; j < i+1; j++){
+                int input = scanner.nextInt();
+                if (j == 1) map[i][j] = map[i-1][j] + input;
+                if (j == i) map[i][j] = map[i-1][j-1] + input;
+                else map[i][j] = Math.max(map[i-1][j-1], map[i-1][j]) + input;
 
                 max = Math.max(max, map[i][j]);
             }
