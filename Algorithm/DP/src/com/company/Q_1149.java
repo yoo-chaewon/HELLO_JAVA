3b9db5d1e5bd9eb6fbd4203e4ee9af81212e9425
yoo-chaewon <ohwow5@sookmyung.ac.kr>
2019-05-30 23:25:26 +0900

    Solve Q_1149

---
+++ Algorithm/DP/src/com/company/Q_1149.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Q_1149 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int H = scanner.nextInt();
+        int[][] map = new int[H+1][5];
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i < H+1; i++){
+            for (int j = 0; j < 3; j++){
+                int input = scanner.nextInt();
+                if (j == 0) map[i][j] = Math.min(map[i-1][1], map[i-1][2]) + input;
+                if (j == 1) map[i][j] = Math.min(map[i-1][0], map[i-1][2]) + input;
+                if (j == 2) map[i][j] = Math.min(map[i-1][0], map[i-1][1]) + input;
+                if (i == H) min = Math.min(min, map[i][j]);
+            }
+        }
+        System.out.println(min);
+    }
+}
