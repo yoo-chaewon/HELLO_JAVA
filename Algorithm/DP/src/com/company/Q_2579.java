74a7d736623396babf11c9571b734ef90a8ecfea
yoo-chaewon <ohwow5@sookmyung.ac.kr>
2019-05-31 14:49:21 +0900

    Solve Q_2579

---
+++ Algorithm/DP/src/com/company/Q_2579.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Q_2579 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int[] map = new int[num+1];
+        int[] sum = new int[num+1];
+        for (int i = 0; i < num; i++){
+            map[i] = scanner.nextInt();
+        }
+        sum[0] = map[0];
+        sum[1] = Math.max(map[1], map[1]+ map[0]);
+        sum[2] = Math.max(map[0] + map[2], map[1] + map[2]);
+
+        for (int i = 3; i < num; i++){
+            sum[i] = Math.max(map[i-1] + sum[i-3] , sum[i-2]) + map[i];
+        }
+        System.out.println(sum[num-1]);
+
+    }
+
+}
