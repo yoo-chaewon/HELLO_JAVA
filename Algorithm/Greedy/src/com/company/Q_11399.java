2e61dfcc1f6c1c997459e4bdcd4199c655537ca5
yoo-chaewon <ohwow5@sookmyung.ac.kr>
2019-06-05 18:49:27 +0900

    Solve Q_11399

--- Algorithm/Greedy/src/com/company/Q_11399.java
+++ Algorithm/Greedy/src/com/company/Q_11399.java
@@ -1,4 +1,25 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Q_11399 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int[] map = new int[size];
+
+        for (int i = 0; i< size; i++){
+            map[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(map);
+        int sum = map[0];
+        for(int i = 0; i < size-1; i++){
+            map[i+1] += map[i];
+            sum += map[i+1];
+        }
+
+        System.out.println(sum);
+    }
 }
