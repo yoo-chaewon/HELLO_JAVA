1d7e243705eb725b9810c7e8538682c8cab963ff
유채원 <ohwow5@sookmyung.ac.kr>
2019-09-02 22:52:12 +0900

    알고리즘 풀어

---
+++ Algorithm/Hash/src/com/company/ProgrammersHash2.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class ProgrammersHash2 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(new InputStreamReader(System.in));
+        int num = scanner.nextInt();
+        String[] phone_book = new String[num];
+
+        for (int i = 0; i < num; i++){
+            phone_book[i] = scanner.next();
+        }
+
+        System.out.print(Solution02.solution(phone_book));
+    }
+}
+
+class Solution02 {
+    public static boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        for (int i = 0; i < phone_book.length-1; i++){
+            for (int j = i+1; j < phone_book.length; j++){
+                if (phone_book[j].startsWith(phone_book[i])){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
