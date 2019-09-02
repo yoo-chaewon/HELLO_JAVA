ac448981f62096cfd2ac06bd7108e73b303984d9
유채원 <ohwow5@sookmyung.ac.kr>
2019-09-02 23:05:06 +0900

    algorithms

--- Algorithm/Hash/src/com/company/ProgrammersHash2.java
+++ Algorithm/Hash/src/com/company/ProgrammersHash2.java
@@ -19,11 +19,9 @@ public class ProgrammersHash2 {
 
 class Solution02 {
     public static boolean solution(String[] phone_book) {
-        boolean answer = true;
-
         for (int i = 0; i < phone_book.length-1; i++){
             for (int j = i+1; j < phone_book.length; j++){
-                if (phone_book[j].startsWith(phone_book[i])){
+                if (phone_book[j].startsWith(phone_book[i]) || phone_book[i].startsWith(phone_book[j])){
                     return false;
                 }
             }
