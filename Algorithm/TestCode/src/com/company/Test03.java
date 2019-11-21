b68a3458bce57f41a5bc2a03fb33c74f23c210b5
유채원 <ohwow5@sookmyung.ac.kr>
2019-11-21 22:26:16 +0900

    solve test 03

--- Algorithm/TestCode/src/com/company/Test03.java
+++ Algorithm/TestCode/src/com/company/Test03.java
@@ -1,4 +1,51 @@
 package com.company;
 
+import java.io.InputStreamReader;
+import java.util.*;
+
+/*
+ * HashMap key-value
+ * 값 넣기
+ * value가 0으로되면 새로운 count++;
+ * */
 public class Test03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(new InputStreamReader(System.in));
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(Solution03.solution(arr));
+    }
+}
+
+class Solution03 {
+    public static int solution(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        HashMap<Integer, Integer> key = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            int flag = -1;
+
+            for (Integer a : map.keySet()) {
+                if (map.get(a) == 0) {
+                    flag = a;
+                }
+            }
+
+            if (flag == -1) {//0인거 없으면
+                key.put(arr[i], 0);
+                map.put(arr[i], map.get(arr[i]) - 1);
+            } else {
+                key.remove(flag);
+                key.put(arr[i], 0);
+            }
+        }
+
+        return key.size();
+    }
 }
