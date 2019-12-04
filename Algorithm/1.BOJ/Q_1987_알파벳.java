f5d50b99b4601ae3cbc1e3e3725bef7afc2ef101
유채원 <ohwow5@sookmyung.ac.kr>
2019-12-04 21:41:30 +0900

    solve Q_1987알파벳

--- Algorithm/1.BOJ/Q_1987_알파벳.java
+++ Algorithm/1.BOJ/Q_1987_알파벳.java
@@ -1,56 +1,58 @@
-package com.company;
-
-import sun.awt.geom.AreaOp;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.HashSet;
 
-public class Q_1987 {
-    static String[][] map;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-    static ArrayList<Integer> result;
+public class Main {
     static int R;
     static int C;
-    static int count;
-    static ArrayList<String > visited;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int result = Integer.MIN_VALUE;
+    static char[][] map;
 
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws Exception{
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = bufferedReader.readLine().split(" ");
-        R = Integer.parseInt(input[0]);//세로
-        C = Integer.parseInt(input[1]);//가로
+        String[] RC = bufferedReader.readLine().split(" ");
 
-        map = new String[R][C];
-        for (int i = 0; i < R; i++) {
-            String[] data = bufferedReader.readLine().split("");
-            for (int j = 0; j < C; j++) {
-                map[i][j] = data[j];
+        R = Integer.parseInt(RC[0]); //세로
+        C = Integer.parseInt(RC[1]); //가
+        map = new char[R][C];
+        visited = new boolean[R][C];
+        HashSet<Character> hashSet = new HashSet<>();
+
+
+        for (int i = 0; i < R; i++){
+            String input = bufferedReader.readLine();
+            for (int j = 0; j < C; j++){
+                map[i][j] = input.charAt(j);
             }
         }
-        visited = new ArrayList<>();
-        result = new ArrayList<>();
-        Alpha_DFS(0, 0);
-        Collections.sort(result);
-        Collections.reverse(result);
-        if (result.size() > 0) {
-            System.out.println(result.get(0));
-        }
+
+        visited[0][0] = true;
+        hashSet.add(map[0][0]);
+        DFS(hashSet,0, 0);
+
+        System.out.println(Math.max(result, hashSet.size()));
+
     }
 
-    public static void Alpha_DFS(int x, int y) {
-        visited.add(map[y][x]);
-        count++;
-        for (int i = 0; i < 4; i++) {
-            int nextX = x + dx[i];
-            int nextY = y + dy[i];
+    public static void DFS(HashSet<Character> hashSet, int x, int y) {
+        for (int i = 0; i < 4; i++){
+            int curX = x + dx[i];
+            int curY = y + dy[i];
 
-            if (nextX < 0 || nextY < 0 || nextX >= C || nextY >= R || visited.contains(map[nextY][nextX])) {
-                result.add(count);
-                continue;
+            if (curX >= 0 && curY >= 0 && curX < C && curY < R && !visited[curY][curX] && !hashSet.contains(map[curY][curX])){
+                visited[curY][curX] = true;
+                hashSet.add(map[curY][curX]);
+                result = Math.max(result, hashSet.size());
+                DFS(hashSet, curX, curY);
+                visited[curY][curX] = false;
+                hashSet.remove(map[curY][curX]);
             }
-            Alpha_DFS(nextX, nextY);
         }
     }
 }
+
+
