d585a9556997e90cd0dfd9c758a1d8c360d5e6e1
유채원 <ohwow5@sookmyung.ac.kr>
2019-12-16 23:57:20 +0900

    SOLVE Q_2667_단지번호붙이기_DFS

--- Algorithm/1.BOJ/Q_2667_단지번호붙이기_DFS.java
+++ Algorithm/1.BOJ/Q_2667_단지번호붙이기_DFS.java
@@ -1,7 +1,10 @@
+package com.company;
+
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Collections;
 
 public class Main {
     static boolean[][] visited;
@@ -34,6 +37,7 @@ public class Main {
             }
         }
         System.out.println(count);
+        Collections.sort(answer);
         for (int a: answer){
             System.out.println(a);
         }
