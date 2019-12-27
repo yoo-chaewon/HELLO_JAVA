2f1e14168038aafe9d21ff9078f0eaf61c869faf
유채원 <ohwow5@sookmyung.ac.kr>
2019-12-27 22:00:11 +0900

    SOLVE Q_10026_적록색약_희수ver

--- Algorithm/1.BOJ/Q_10026_적록색약_DFS_ver2.java
+++ Algorithm/1.BOJ/Q_10026_적록색약_DFS_ver2.java
@@ -1,27 +1,67 @@
-package com.company;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Q_1003_DP {
-    static int[][] map;
-    public static void main(String[] args) throws Exception{
+public class Main {
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int size;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int test_case = Integer.parseInt(bufferedReader.readLine());
-        while (test_case -- > 0){
-            int num = Integer.parseInt(bufferedReader.readLine());
-            map = new int[num+2][2];
+        size = Integer.parseInt(bufferedReader.readLine());
+        char[][] map1 = new char[size][size];
+        char[][] map2 = new char[size][size];
 
-            map[0][0] = 1;
-            map[1][1] = 1;
+        for (int i = 0; i < size; i++) {
+            String temp = bufferedReader.readLine();
+            for (int j = 0; j < size; j++) {
+                map1[i][j] = temp.charAt(j);
 
-            for (int i = 2; i < num+1; i++){
-                for (int j = 0; j < 2; j++){
-                    map[i][j] = map[i-1][j] + map[i-2][j];
+                if (temp.charAt(j) == 'R'){
+                    map2[i][j] = 'G';
+                }else {
+                    map2[i][j] = temp.charAt(j);
                 }
             }
+        }
 
-            System.out.println(map[num][0] + " " + map[num][1]);
+        int count1 = 0;
+        boolean[][] visited1 = new boolean[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (!visited1[i][j]) {
+                    DFS(map1, visited1, j, i);
+                    count1++;
+                }
+            }
+        }
+
+        int count2 = 0;
+        boolean[][] visited2 = new boolean[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (!visited2[i][j]) {
+                    DFS(map2, visited2, j, i);
+                    count2++;
+                }
+            }
+        }
+        System.out.println(count1 +" " + count2);
+    }
+
+    public static void DFS(char[][] map, boolean[][] visited, int x, int y) {
+        visited[y][x] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int curX = x + dx[i];
+            int curY = y + dy[i];
+
+            if (curX >= size || curY >= size || curX < 0 || curY < 0 || visited[curY][curX]) continue;
+
+            if (map[y][x] == map[curY][curX]) {
+                DFS(map,visited, curX, curY);
+            }
         }
     }
 }
+
