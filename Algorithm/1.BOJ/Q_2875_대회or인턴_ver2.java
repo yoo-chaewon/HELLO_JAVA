4f3afc99e354f4db66705727d2c24986fbc0d056
유채원 <ohwow5@sookmyung.ac.kr>
2020-01-08 15:35:54 +0900

    solve Q_2875_대회or인턴

--- Algorithm/1.BOJ/Q_2875_대회or인턴_ver2.java
+++ Algorithm/1.BOJ/Q_2875_대회or인턴_ver2.java
@@ -1,32 +1,20 @@
-package com.company;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Q_1003 {
-    static int[] arr;
-    static int num0 = 0;
-    static int num1 = 0;
-    public static void main(String[] args) throws Exception{
+public class Main {
+    public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int test_case = Integer.parseInt(bufferedReader.readLine());
-        while (test_case-- > 0){
-            int num = Integer.parseInt(bufferedReader.readLine());
-            arr = new int[num+1];
-            fibonacci(num);
-            System.out.println(num0 + " " + num1);
-            num0 = 0; num1 = 0;
+        String[] input = bufferedReader.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+        int K = Integer.parseInt(input[2]);
+        int total = N + M;
+
+        int team = M;
+        while (true){
+            if (K <=total-3*team && team*2 <= N) break;
+            team --;
         }
-    }
-    public static int fibonacci(int x){
-        if (x == 0) {
-            num0++;
-            return 0;
-        }
-        if (x == 1) {
-            num1++;
-            return  1;
-        }
-        else return fibonacci(x-1) + fibonacci(x-2);
+        System.out.println(team);
     }
 }
