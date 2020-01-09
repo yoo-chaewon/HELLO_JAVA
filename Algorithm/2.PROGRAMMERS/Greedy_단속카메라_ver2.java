06f80dd460ebcb84a4b8efbdfbab53b4113f50a0
유채원 <ohwow5@sookmyung.ac.kr>
2020-01-10 00:33:01 +0900

    Update Greedy_단속카메라

--- Algorithm/2.PROGRAMMERS/Greedy_단속카메라_ver2.java
+++ Algorithm/2.PROGRAMMERS/Greedy_단속카메라_ver2.java
@@ -3,12 +3,8 @@ class Solution {
     public int solution(int[][] routes) {
         int answer = 1;
 
-        Arrays.sort(routes, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0]-o2[0];
-            }
-        });
+        Arrays.sort(routes, (a,b) -> Integer.compare(a[0], b[0]));
+
 
         int end = routes[0][1];
         for (int i = 0; i < routes.length-1; i++){
