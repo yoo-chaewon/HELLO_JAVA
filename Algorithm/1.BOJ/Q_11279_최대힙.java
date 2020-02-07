7c47bdd6e6808a121e5bb9319ebf6c9c18a1c596
유채원 <ohwow5@sookmyung.ac.kr>
2020-02-07 13:33:02 +0900

    Solve BOJ_11279_최대힙(#7)
    최대힙 문제
    PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>(){
    	@override
    	public int compare(Integer o1, Integer o2){
    		return o2-o1;
    	}
    });

---
+++ Algorithm/1.BOJ/Q_11279_최대힙.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int size = Integer.parseInt(bufferedReader.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        });
+
+        while (size -- > 0){
+            int num = Integer.parseInt(bufferedReader.readLine());
+            if (num == 0){
+                if (queue.isEmpty()) System.out.println(0);
+                else System.out.println(queue.poll());
+            }else{
+                queue.add(num);
+            }
+        }
+    }
+}
