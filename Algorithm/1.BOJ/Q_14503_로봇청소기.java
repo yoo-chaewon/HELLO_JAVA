5d33b5bf01e3fa7d3bcf5fb4f35e989aa3c3a456
유채원 <ohwow5@sookmyung.ac.kr>
2020-02-09 14:52:25 +0900

    Change file location

--- Algorithm/1.BOJ/Q_14503_로봇청소기.java
+++ Algorithm/1.BOJ/Q_14503_로봇청소기.java
@@ -2,54 +2,53 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    //북동남서
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {-1, 0, 1, 0};
-
     static int N, M;
     static int[][] map;
     static int count = 1;
+
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String[] NM = bufferedReader.readLine().split(" ");
-        N = Integer.parseInt(NM[0]);
-        M = Integer.parseInt(NM[1]);
+        N = Integer.parseInt(NM[0]);//세로
+        M = Integer.parseInt(NM[1]);//가로
 
         String[] input = bufferedReader.readLine().split(" ");
-        int r = Integer.parseInt(input[0]);
-        int c = Integer.parseInt(input[1]);
+        int startY = Integer.parseInt(input[0]);
+        int startX = Integer.parseInt(input[1]);
         int d = Integer.parseInt(input[2]);
-
         map = new int[N][M];
-        for (int i = 0; i < N; i++){
-            String[] line = bufferedReader.readLine().split(" ");
-            for (int j = 0; j < M; j++){
-                map[i][j] = Integer.parseInt(line[j]);
+
+        for (int i = 0; i < N; i++) {
+            String[] tmp = bufferedReader.readLine().split(" ");
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(tmp[j]);
             }
         }
-
-        DFS(c, r, d);
+        DFS(startX, startY, d);
         System.out.println(count);
     }
 
-    public static void DFS(int x, int y, int dir){
-        map[y][x] = 5;//visited
+    static void DFS(int x, int y, int dir) {
+        map[y][x] = 5;
 
-        for (int i = 0; i < 4; i++){
-            dir = ((3+dir) ) % 4;
+        for (int i = 0; i < 4; i++) {
+            dir = (dir + 3) % 4;
             int nextX = x + dx[dir];
             int nextY = y + dy[dir];
 
-            if (nextX < 0 || nextY < 0 || M <= nextX || N <= nextY || map[nextY][nextX] != 0) continue;
+            if (nextX < 0 || nextY < 0 || nextX >= M || nextY >= N || map[nextY][nextX] != 0) continue;
             count++;
             DFS(nextX, nextY, dir);
             return;
         }
 
-        int backDir = (dir + 2) % 4;
+        int backDir = (dir + 2) % 4;//초기화
         int backX = x + dx[backDir];
         int backY = y + dy[backDir];
-        if (0 <= backX && 0 <= backY && backX < M && backY < N && map[backY][backX] != 1){
+
+        if (backX >= 0 && backY >= 0 && backX < M && backY < N && map[backY][backX] != 1) {
             DFS(backX, backY, dir);
         }
     }
