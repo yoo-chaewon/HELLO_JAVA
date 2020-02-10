c64c9be6caf09e72d3bd9091c3f9372fd8b0b421
유채원 <ohwow5@sookmyung.ac.kr>
2020-02-11 01:06:42 +0900

    Update BOJ_4195_친구네트워크

--- Algorithm/1.BOJ/Q_4195_친구네트워크.java
+++ Algorithm/1.BOJ/Q_4195_친구네트워크.java
@@ -4,29 +4,26 @@ import java.util.HashMap;
 
 public class Main {
     static int MAX_SIZE = 200001;
-    static int index = 1;
     static int[] parent;
-    static int[] level;
     static int[] relation;
-
+    static int[] level;
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int testCase = Integer.parseInt(bufferedReader.readLine());
+        int T = Integer.parseInt(bufferedReader.readLine());
 
-        while (testCase-- > 0) {
-            StringBuilder stringBuilder = new StringBuilder();
+        while (T-- > 0) {
             int f = Integer.parseInt(bufferedReader.readLine());
-            HashMap<String, Integer> hash = new HashMap<>();
             parent = new int[MAX_SIZE];
-            level = new int[MAX_SIZE];
             relation = new int[MAX_SIZE];
-
-            for (int i = 1; i < MAX_SIZE; i++) {
+            level = new int[MAX_SIZE];
+            HashMap<String, Integer> hash = new HashMap<>();
+            int index = 1;
+            for (int i = 1; i < MAX_SIZE; i++){
                 parent[i] = i;
                 relation[i] = 1;
             }
 
-            while (f-- > 0) {
+            for (int i = 0; i < f; i++){
                 String[] input = bufferedReader.readLine().split(" ");
                 String a = input[0];
                 String b = input[1];
@@ -38,44 +35,42 @@ public class Main {
                 if (!hash.containsKey(b)){
                     hash.put(b, index++);
                 }
+
                 int aIndex = hash.get(a);
                 int bIndex = hash.get(b);
-
-                Merge(aIndex, bIndex);
+                merge(aIndex, bIndex);
             }
         }
     }
 
-    public static void Merge(int a, int b){
-        a = find(a);
-        b = find(b);
-
+    public static void merge(int a, int b){
+        a = findParent(a);
+        b = findParent(b);
         if (a == b){
             System.out.println(relation[a]);
             return;
         }
 
-        //b가 더 높다고 가정
-        if (level[a] > level[b]){
+        //a 가 더 높아야 한다고 가정.
+        if (level[a] < level[b]){
             int tmp = a;
             a = b;
             b = tmp;
         }
-
-        parent[a] = b;
-        relation[b] += relation[a];
-        System.out.println(relation[b]);
+        parent[b] = a;
+        relation[a] += relation[b];
+        level[a]++;
+        System.out.println(relation[a]);
 
         if (level[a] == level[b]){
-            level[b]++;
+            level[a]++;
         }
     }
 
-    //parent node 찾는 메소드
-    public static int find(int u){
-        if (u == parent[u]){
-            return u;
+    public static int findParent(int a){
+        if (a == parent[a]){
+            return a;
         }
-        return parent[u] = find(parent[u]);
+        return parent[a] = findParent(parent[a]);
     }
 }
