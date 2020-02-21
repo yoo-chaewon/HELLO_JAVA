c31d0777db0d8504c006e28627a3d792c0560181
유채원 <ohwow5@sookmyung.ac.kr>
2020-02-21 14:13:44 +0900

    Solve Programmers_Greedy_큰수만들기_91.7
    앞에서 부터 보면서 다음 값보다 작은 경우 지워줌
    테스트 10번케이스 시간초과

--- Algorithm/2.PROGRAMMERS/with Study/Greedy_큰수만들기_91.7_시간초과.java
+++ Algorithm/2.PROGRAMMERS/with Study/Greedy_큰수만들기_91.7_시간초과.java
@@ -5,20 +5,20 @@ class Solution {
         StringBuilder answer = new StringBuilder();
         ArrayList<Integer> arrayList = new ArrayList<>();
 
-        for (int i = 0; i < number.length(); i++){
+        for (int i = 0; i < number.length(); i++) {
             arrayList.add(number.charAt(i) - '0');
         }
 
-        for (int i = 0; i < k; i++){
-            for (int j = 0; j < arrayList.size()-1; j++){
-                if (arrayList.get(j) < arrayList.get(j+1)){
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < arrayList.size() - 1; j++) {
+                if (arrayList.get(j) < arrayList.get(j + 1)) {
                     arrayList.remove(j);
                     break;
                 }
             }
         }
 
-        for (int i =0; i < number.length()-k; i++) answer.append(arrayList.get(i));
+        for (int i = 0; i < number.length() - k; i++) answer.append(arrayList.get(i));
         return answer.toString();
     }
 }
