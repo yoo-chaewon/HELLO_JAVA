ddd63de56c30fabe26721c7b5e9b05654d9d328d
유채원 <ohwow5@sookmyung.ac.kr>
2020-03-16 14:48:22 +0900

    Solve Programmers_DFS/BFS_단어변환(#9)

---
+++ Algorithm/2.PROGRAMMERS/with Study/BFS:DFS_단어변환.java
@@ -0,0 +1,39 @@
+class Solution {
+    static int answer = Integer.MAX_VALUE;
+    static String mtarget;
+    static String[] mwords;
+    static boolean[] visited;
+    static boolean flag = false;
+
+    public static int solution(String begin, String target, String[] words) {
+        mtarget = target;
+        mwords = words;
+        visited = new boolean[words.length];
+        DFS(begin, 0, 0, begin + " ");
+        if (flag) return answer;
+        else return 0;
+    }
+
+    public static void DFS(String cur, int indx, int count, String result) {
+        if (cur.equals(mtarget)) {
+//            System.out.println(result);
+            answer = Math.min(count, answer);
+            flag = true;
+            return;
+        }
+
+        for (int i = 0; i < mwords.length; i++) {
+            if (!visited[i]) {
+                int tmp = 0;
+                for (int j = 0; j < mtarget.length(); j++) {
+                    if (cur.charAt(j) == mwords[i].charAt(j)) tmp++;
+                }
+                if (tmp >= mtarget.length()-1) {
+                    visited[i] = true;
+                    DFS(mwords[i], i + 1, count + 1, result + mwords[i] + " ");
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+}
