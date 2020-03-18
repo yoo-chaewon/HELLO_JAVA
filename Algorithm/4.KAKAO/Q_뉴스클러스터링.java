74bce7f50306c85d71ee9c25f9ca401762b55c52
유채원 <ohwow5@sookmyung.ac.kr>
2020-03-18 16:03:49 +0900

    Update Programmers_뉴스클러스터링

--- Algorithm/4.KAKAO/Q_뉴스클러스터링.java
+++ Algorithm/4.KAKAO/Q_뉴스클러스터링.java
@@ -2,46 +2,44 @@ import java.util.HashMap;
 
 class Solution {
     static double VALUE = 65536;
-
     public static int solution(String str1, String str2) {
-        str1 = str1.toLowerCase();
-        str2 = str2.toLowerCase();
+        HashMap<String, Integer> str1Hash = MakeHash(str1);
+        HashMap<String, Integer> str2Hash = MakeHash(str2);
+
+        if (str1Hash.size() == 0 && str2Hash.size() == 0) return (int)VALUE;
         int sum = 0;
         int intersection = 0;
 
-        HashMap<String, Integer> str1Hash = new HashMap<>();
-        for (int i = 0; i < str1.length() - 1; i++) {
-            if (str1.charAt(i) >= 'a' && str1.charAt(i) <= 'z'
-                    && str1.charAt(i + 1) >= 'a' && str1.charAt(i + 1) <= 'z') {
-                String key = str1.substring(i, i+2);
-                str1Hash.put(key, str1Hash.getOrDefault(key, 0) + 1);
-                sum++;
+        for (String key : str1Hash.keySet()){
+            if (str2Hash.containsKey(key)){
+                sum += Math.max(str1Hash.get(key) , str2Hash.get(key));
+                continue;
             }
-        }
-        HashMap<String, Integer> str2Hash = new HashMap<>();
-        for (int i = 0; i < str2.length() - 1; i++) {
-            if (str2.charAt(i) >= 'a' && str2.charAt(i) <= 'z'
-                    && str2.charAt(i + 1) >= 'a' && str2.charAt(i + 1) <= 'z') {
-                String key = str2.substring(i, i+2);
-                str2Hash.put(key, str2Hash.getOrDefault(key, 0) + 1);
-            }
+            sum+=str1Hash.get(key);
         }
 
-        if (str1Hash.size() == 0 && str2Hash.size() == 0) return (int)VALUE;
         for (String key : str2Hash.keySet()){
             if (str1Hash.containsKey(key)){
                 intersection += Math.min(str1Hash.get(key) , str2Hash.get(key));
-
-                if (str1Hash.get(key) < str2Hash.get(key)){
-                    sum -= str1Hash.get(key);
-                    sum += str2Hash.get(key);
-                }
-            }else {
-                sum += str2Hash.get(key);
+                continue;
             }
+            sum += str2Hash.get(key);
         }
+
         double temp  = (double)intersection/ (double)sum  * VALUE;
         return (int) temp;
     }
-}
 
+    public static HashMap<String, Integer> MakeHash(String str){
+        str = str.toLowerCase();
+        HashMap<String, Integer> hash = new HashMap<>();
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z'
+                    && str.charAt(i + 1) >= 'a' && str.charAt(i + 1) <= 'z') {
+                String key = str.substring(i, i+2);
+                hash.put(key, hash.getOrDefault(key, 0) + 1);
+            }
+        }
+        return hash;
+    }
+}
