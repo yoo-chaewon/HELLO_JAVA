50739665e8743e5e2f53bb4284054a2b3e53e222
유채원 <ohwow5@sookmyung.ac.kr>
2020-04-01 12:42:17 +0900

    [Update] Programmers Q_종이접기
    solution 함수 올림

--- Algorithm/2.PROGRAMMERS/with Study/Q_종이접기.java
+++ Algorithm/2.PROGRAMMERS/with Study/Q_종이접기.java
@@ -1,9 +1,18 @@
 class Solution {
     static int[] answer;
+    public static int[] solution(int n) {
+        answer = new int[(int) (Math.pow(2, n) - 1)];
+        if(n != 1){
+            make((int) (Math.pow(2, n) - 1), 1);
+        }
+        return answer;
+    }
+
     public static void make(int size, int cur) {
         if (size / 2 == cur) {
             for (int i = 1; i <= cur; i++) {
                 answer[cur+i] = answer[cur-i] == 1? 0: 1;
+
             }
             return;
         }
