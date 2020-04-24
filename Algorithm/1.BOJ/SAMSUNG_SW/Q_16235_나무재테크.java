f28110a532c320a48830fdd9dc120e02682e6958
유채원 <ohwow5@sookmyung.ac.kr>
2020-04-24 20:08:18 +0900

    Update BOJ16235_나무재테크
    입력 좌표 확인 잘하기
    각각의 칸은 (r, c)로 나타내며, r은 가장 위에서부터 떨어진 칸의 개수, c는 가장 왼쪽으로부터 떨어진 칸의 개수이다. r과 c는 1부터 시작한다.
    
    라고 주어졌으니까,
    나무의 정보를 나타내는 세 정수 x, y, z가 주어진다. 처음 두 개의 정수는 나무의 위치 (x, y)를 의미
    
    라고 입력 조건이 주어져도, 나의 좌표계에서는 입력을
    y,x,size로 생각해야 한다 .

--- Algorithm/1.BOJ/SAMSUNG_SW/Q_16235_나무재테크.java
+++ Algorithm/1.BOJ/SAMSUNG_SW/Q_16235_나무재테크.java
@@ -1,95 +1,94 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
     static int N;
-    static int[][] addMap;
-    static int[][] groundMap;
+    static int[][] ground;
+    static int[][] machine;
+    static PriorityQueue<Tree> treeQueue = new PriorityQueue<>(new Comparator<Tree>() {
+        @Override
+        public int compare(Tree o1, Tree o2) {
+            return o1.size - o2.size;
+        }
+    });
+    static Queue<Tree> live = new LinkedList<>();
+    static Queue<Tree> death = new LinkedList<>();
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer input = new StringTokenizer(bufferedReader.readLine());
+        N = Integer.parseInt(input.nextToken());
+        int M = Integer.parseInt(input.nextToken());
+        int K = Integer.parseInt(input.nextToken());
 
-    static Queue<Point> save = new LinkedList<>();
-    static Queue<Point> death = new LinkedList<>();
-    static PriorityQueue<Point> treeQueue;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        int M = sc.nextInt();
-        int K = sc.nextInt();
-        addMap = new int[N][N];
-        groundMap = new int[N][N];
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                addMap[i][j] = sc.nextInt();
-                groundMap[i][j] = 5;
+        ground = new int[N][N];
+        machine = new int[N][N];
+        for (int i = 0; i < N; i++){
+            StringTokenizer str = new StringTokenizer(bufferedReader.readLine());
+            for (int j = 0; j < N; j++){
+                machine[i][j] = Integer.parseInt(str.nextToken());
+                ground[i][j] = 5;
             }
         }
 
-        treeQueue = new PriorityQueue<Point>(new Comparator<Point>() {
-            @Override
-            public int compare(Point o1, Point o2) {
-                return o1.age - o2.age;
-            }
-        });
-
-        for (int i = 0; i < M; i++) {
-            treeQueue.add(new Point(sc.nextInt() - 1, sc.nextInt() - 1, sc.nextInt()));
+        while (M-- >0){
+            StringTokenizer str = new StringTokenizer(bufferedReader.readLine());
+            int y = Integer.parseInt(str.nextToken())-1;
+            int x = Integer.parseInt(str.nextToken())-1;
+            treeQueue.add(new Tree(x, y, Integer.parseInt(str.nextToken())));
         }
 
-        for (int i = 0; i < K; i++) year();
+        while (K-->0) Year();
         System.out.println(treeQueue.size());
     }
 
-    static int[] dx = {1, 1, 1, -1, -1, -1, 0, 0};
-    static int[] dy = {-1, 0, 1, -1, 0, 1, -1, 1};
-
-    private static void year() {
+    static int[] dx = {-1, -1, -1, 0, 1, 1, 1, 0};
+    static int[] dy = {-1, 0 , 1, 1, 1, 0, -1, -1};
+    public static void Year(){
         //봄
-        while (!treeQueue.isEmpty()) {
-            Point cur = treeQueue.poll();
-            if (groundMap[cur.x][cur.y] >= cur.age) {
-                groundMap[cur.x][cur.y] -= cur.age;
-                cur.age += 1;
-                save.add(cur);
-            } else {
-                death.add(cur);
-            }
+        while (!treeQueue.isEmpty()){
+            Tree cur = treeQueue.poll();
+            if (ground[cur.y][cur.x] >= cur.size) {
+                ground[cur.y][cur.x] -= cur.size;
+                live.add(new Tree(cur.x, cur.y, cur.size + 1));
+            }else death.add(cur);
         }
 
         //여름
-        while (!death.isEmpty()) {
-            Point cur = death.poll();
-            groundMap[cur.x][cur.y] += cur.age / 2;
+        while (!death.isEmpty()){
+            Tree cur = death.poll();
+            ground[cur.y][cur.x] += cur.size/2;
         }
 
         //가을
-        while (!save.isEmpty()) {
-            Point cur = save.poll();
-            if (cur.age % 5 == 0) {
-                for (int i = 0; i < 8; i++) {
+        while (!live.isEmpty()){
+            Tree cur = live.poll();
+            treeQueue.add(cur);
+            if (cur.size % 5 == 0){
+                for (int i = 0; i < 8; i++){
                     int nextX = cur.x + dx[i];
                     int nextY = cur.y + dy[i];
-                    if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= N) continue;
-                    treeQueue.add(new Point(nextX, nextY, 1));
 
+                    if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= N) continue;
+                    treeQueue.add(new Tree(nextX, nextY, 1));
                 }
             }
-            treeQueue.add(cur);
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                groundMap[i][j] += addMap[i][j];
+        //겨울
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
+                ground[i][j] += machine[i][j];
             }
         }
     }
 }
 
-class Point {
-    int x, y, age;
-
-    Point(int x, int y, int age) {
+class Tree{
+    int x, y, size;
+    Tree(int x, int y, int size){
         this.x = x;
         this.y = y;
-        this.age = age;
+        this.size = size;
     }
 }
