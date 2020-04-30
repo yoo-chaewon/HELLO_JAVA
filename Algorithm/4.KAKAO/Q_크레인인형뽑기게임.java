ce87e20e38e65ab60e711056968d1dc6376bbeb4
유채원 <ohwow5@sookmyung.ac.kr>
2020-04-30 23:12:48 +0900

    Update KAKAO_크레인인형뽑기게임

--- Algorithm/4.KAKAO/Q_크레인인형뽑기게임.java
+++ Algorithm/4.KAKAO/Q_크레인인형뽑기게임.java
@@ -1,25 +1,21 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        Stack<Integer>[] board_stack = new Stack[board.length];
-        for (int i = 0; i < board.length; i++){
-            board_stack[i] = new Stack<>();
-        }
-        for (int i = board.length-1; i >= 0 ; i--){
-            for (int j = 0; j < board.length; j++){
-                if (board[i][j] != 0) board_stack[j].add(board[i][j]);
-            }
-        }
-        int answer = 0;
         Stack<Integer> basket = new Stack<>();
-        for (int move : moves){
-            if (!board_stack[move-1].isEmpty()){
-                int cur = board_stack[move-1].pop();
-                if (!basket.isEmpty() && cur == basket.peek()){
-                    answer+=2;
-                    basket.pop();
-                }else {
-                    basket.push(cur);
+        int answer = 0;
+
+        for (int move : moves) {
+            for (int i = 0; i < board.length; i++) {
+                if (board[i][move - 1] != 0) {
+                    if (!basket.isEmpty() && board[i][move - 1] == basket.peek()) {
+                        answer += 2;
+                        basket.pop();
+                    } else {
+                        basket.push(board[i][move - 1]);
+                    }
+                    board[i][move - 1] = 0;
+                    break;
                 }
             }
         }
