a350833dc752a2aef4119e71a84eb84775edd21f
유채원 <ohwow5@sookmyung.ac.kr>
2020-04-30 15:59:02 +0900

    Solve KAKAO_호텔방배정

---
+++ Algorithm/4.KAKAO/Q_호텔방배정.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Solution {
+    public long[] solution(long k, long[] room_number) {
+        LinkedHashSet<Long> result = new LinkedHashSet<>();
+        for (long number : room_number){
+            if (!result.contains(number)){
+                result.add(number);
+            }else {
+                for (long i = number+1; i < k; i++){
+                    if (!result.contains(i)){
+                        result.add(i);
+                        break;
+                    }
+                }
+            }
+        }
+        long[] answer = new long[room_number.length];
+        int cur = 0;
+        for (long key : result){
+            answer[cur++] = key;
+            if (cur == room_number.length) break;
+        }
+        return answer;
+    }
+}
